cc8ed641f1e5670e4d09206637b0f703a5491b63
ThimiraPathirana <36132763+ThimiraPathirana@users.noreply.github.com>
2024-09-20 23:31:31 +0530

    lazi loading two type example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
+import { AddUserComponent } from './admin/add-user/add-user.component';
 
 export const routes: Routes = [
+    /** lazi loading can create as 1.loadChildren & 2.loadComponent */
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    },
+    {
+        path: "add-user",
+        loadComponent: () => import('./admin/add-user/add-user.component').then(m=> m.AddUserComponent)
+    },
+    /** lazi loading end */
+
+    /** normal component add to route 
+    {
+        path: "add-user",
+        component: AddUserComponent
     }
+     without lazi loading end */
 ];
